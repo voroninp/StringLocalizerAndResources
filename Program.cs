dd1e1bb8947c23bc7e3932b2cf53bfbdd2807384
Pavel Voronin <pavel@mediabuyingsystems.com>
2024-07-16 17:56:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using StringLocalizerAndResources.Resources;
+
+var services = new ServiceCollection();
+services.AddLocalization(cfg => cfg.ResourcesPath = "").AddLogging();
+
+var sp = services.BuildServiceProvider();
+var typedLocilizer = sp.GetRequiredService<IStringLocalizer<Foo>>();
+Console.WriteLine(typedLocilizer["Key"]);
+
+var locilizer = sp.GetRequiredService<IStringLocalizerFactory>().Create("res", Assembly.GetExecutingAssembly().FullName!);
+Console.WriteLine(locilizer["Key"]);
